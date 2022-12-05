13747c40c3f199de1316bd17006ec4eaff9a0204
zhangbowy <1751170889@qq.com>
2022-12-05 09:57:07 +0800

    feat: 新增console信息存储的实体

---
+++ src/service/console.service.ts
@@ -0,0 +1,46 @@
+import {InjectEntityModel} from '@midwayjs/typegoose';
+import {ReturnModelType} from '@typegoose/typegoose';
+import {Console} from '../entity/console';
+import {Provide} from '@midwayjs/decorator';
+
+
+/**
+ * 控制台信息server
+ */
+@Provide()
+export class ConsoleService {
+
+    @InjectEntityModel(Console)
+    consoleModel: ReturnModelType<typeof Console>;
+
+    /**
+     * 添加控制台信息
+     */
+    async addConsole(): Promise<string> {
+        const {_id: id} = await this.consoleModel.create({
+            level: 'info',
+            content: {
+                0: '222',
+                1: '2'
+            },
+            timestamp: new Date().getTime(),
+            createTime: new Date()
+        } as unknown as Console); // an "as" assertion, to have types for all properties
+        return id;
+    }
+
+
+
+    async findConsole() {
+        return this.consoleModel.find().exec();
+    }
+
+    async getTest() {
+        // create data
+        const {_id: id} = await this.consoleModel.create({name: 'JohnDoe', jobs: ['Cleaner']} as Console); // an "as" assertion, to have types for all properties
+
+        // find data
+        const user = await this.consoleModel.findById(id).exec();
+        console.log(user)
+    }
+}
